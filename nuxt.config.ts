6e9485c900dcaa167271782d6ebb854f4a30e941
Sabrina Ferguson <sf@matterlabs.dev>
2024-04-04 17:01:52 -0400

    init Nuxt project

---
+++ nuxt.config.ts
@@ -0,0 +1,62 @@
+import { getIconCollections } from '@egoist/tailwindcss-icons';
+import { zksyncIcons } from './assets/zksync-icons';
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: ['@nuxt/ui-pro'],
+  modules: ['@nuxt/content', '@nuxt/ui', '@nuxt/fonts', '@nuxthq/studio', 'nuxt-og-image'],
+  hooks: {
+    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
+    'components:extend': (components) => {
+      const globals = components.filter((c) => ['UButton', 'UIcon', 'UKbd'].includes(c.pascalName));
+
+      globals.forEach((c) => (c.global = true));
+    },
+  },
+  ui: {
+    icons: {
+      collections: {
+        ...zksyncIcons,
+        ...getIconCollections(['heroicons', 'simple-icons']),
+      },
+    },
+  },
+  routeRules: {
+    '/api/search.json': { prerender: true },
+  },
+  devtools: {
+    enabled: true,
+  },
+  typescript: {
+    strict: false,
+  },
+  content: {
+    highlight: {
+      langs: [
+        'solidity',
+        'go',
+        'jsx',
+        'swift',
+        'python',
+        'rust',
+        'toml',
+        'java',
+        'xml',
+        'graphql',
+        'groovy',
+        'asm',
+        'c++',
+      ],
+      theme: {
+        default: 'github-light',
+        light: 'github-light',
+        dark: 'github-dark',
+        sepia: 'monokai',
+      },
+    },
+  },
+  nitro: {
+    firebase: {
+      gen: 2,
+    },
+  },
+});
