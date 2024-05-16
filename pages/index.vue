459d8a78dbbaea245344db9ecf36c25981be98c9
Sabrina Ferguson <sf@matterlabs.dev>
2024-05-16 10:19:55 -0400

    chore: fix links

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// useSeoMeta({
+//   titleTemplate: '%s - Nuxt UI Pro',
+//   ogImage: 'https://ui.nuxt.com/social-card-pro.png',
+//   twitterImage: 'https://ui.nuxt.com/social-card-pro.png',
+// });
+</script>
+
+<template>
+  <NuxtPage />
+</template>
