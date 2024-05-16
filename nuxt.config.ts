a7cd45704681fdf0ecf781622e151966ed8e61a3
Sabrina Ferguson <sf@matterlabs.dev>
2024-05-16 15:03:37 -0400

    chore: update theme and config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   extends: [['github:matter-labs/docs-nuxt-template', { install: true }]],
   modules: ['@nuxt/content', '@nuxt/ui', '@nuxt/eslint'],
+  site: {
+    name: 'zkSync SDK Docs',
+    url: process.env.NUXT_SITE_ENV ? 'https://staging-sdk-docs.zksync.io' : 'https://docs.zksync.io',
+  },
   runtimeConfig: {
     public: {
       app: 'sdk',
