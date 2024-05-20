35c1ad565b0787e71697a9f6838529e9bffa857b
Sabrina Ferguson <sf@matterlabs.dev>
2024-05-20 17:51:40 -0400

    chore: get sdk working

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+useHead({
+  meta: [
+    {
+      'http-equiv': 'refresh',
+      content: '0; url=/sdk',
+    },
+  ],
+});
+</script>
+
+<template>
+  <UPageError
+    :status="200"
+    name="SDK Docs"
+    message="This is not the page you are looking for. 👋"
+    :clear-button="{
+      label: 'Go to SDK Landing Page',
+      to: '/sdk',
+    }"
+  />
+</template>
