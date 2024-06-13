692208a85e94e8f66ca8202f1dd5ed59b4b828c4
Ngozi-Txfusion <ngozi@txfusion.io>
2024-06-13 08:04:58 +0100

    chore: minor uodate

--- layouts/sdk.vue
+++ layouts/sdk.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import type { NavItem } from '@nuxt/content/dist/runtime/types';
-import SdkSelector from '~/component/sdk-selector.vue';
 
 const nav = inject<Ref<NavItem[]>>('navigation', ref([]));
 
@@ -12,9 +11,6 @@ const navigation = computed(() => nav!.value.find((item) => item._path.startsWit
     <UPage>
       <template #left>
         <UAside>
-          <div class="mb-6">
-            <SdkSelector />
-          </div>
           <UNavigationTree
             :links="mapContentNavigation(navigation)"
             default-open
