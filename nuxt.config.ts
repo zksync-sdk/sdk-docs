aafb93b43411be145f631c3d04d3f80dd62fac33
Sabrina Ferguson <sf@matterlabs.dev>
2024-06-27 15:26:14 -0400

    feat: use @matterlabs/docs-nuxt-layer npm pkg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: [['github:matter-labs/docs-nuxt-template', { install: true }]],
+  extends: [['@matterlabs/docs-nuxt-template']],
   modules: ['@nuxt/content', '@nuxt/ui', '@nuxt/eslint', '@nuxtjs/seo'],
   site: {
     name: 'ZKsync SDK Docs',
