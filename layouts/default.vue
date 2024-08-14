60ad879cd5ca5055e69e08dfb4a0109456149347
Sabrina Ferguson <sf@matterlabs.dev>
2024-08-14 14:34:47 -0400

    feat: update sdk to subdomain

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { NavItem } from '@nuxt/content/dist/runtime/types';
+
+const nav = inject<Ref<NavItem[]>>('navigation', ref([]));
+
+// const navigation = computed(() => nav!.value.find((item) => item._path.startsWith('/'))?.children || []);
+const navigation = computed(() => nav!.value || []);
+</script>
+
+<template>
+  <UContainer>
+    <UPage>
+      <template #left>
+        <UAside>
+          <UNavigationTree
+            :links="mapContentNavigation(navigation)"
+            default-open
+            :multiple="false"
+          />
+        </UAside>
+      </template>
+      <slot />
+    </UPage>
+  </UContainer>
+</template>
---
+++ pages/[...slug].vue
@@ -0,0 +1,122 @@
+<script setup lang="ts">
+import type { ParsedContent } from '@nuxt/content/types';
+import { withoutTrailingSlash } from 'ufo';
+
+const route = useRoute();
+const { toc, seo } = useAppConfig();
+
+const { data: page } = await useAsyncData(route.path, () => queryContent(route.path).findOne());
+if (!page.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true });
+}
+
+const { data: surround } = await useAsyncData(
+  `${route.path}-surround`,
+  () =>
+    queryContent()
+      .where({ _partial: false, _extension: 'md', navigation: { $ne: false } })
+      .only(['title', 'description', '_path'])
+      .findSurround(withoutTrailingSlash(route.path)),
+  { default: () => [] }
+);
+
+useSeoMeta({
+  title: page.value.title,
+  ogTitle: `${page.value.title} - ${seo?.siteName}`,
+  description: page.value.description,
+  ogDescription: page.value.description,
+  twitterDescription: page.value.description,
+});
+
+defineOgImage({
+  component: 'OgImageZK',
+  title: page.value.title,
+  description: page.value.description,
+});
+
+const headline = computed(() => findPageHeadline(page.value as ParsedContent));
+
+const links = computed(() =>
+  [
+    {
+      icon: 'i-heroicons-pencil-square',
+      label: 'Edit this page',
+      to: `https://github.com/zksync-sdk/sdk-docs/edit/main/content/${page?.value?._file}`,
+      target: '_blank',
+    },
+    {
+      icon: 'i-heroicons-chat-bubble-oval-left-ellipsis',
+      label: 'Share feedback',
+      to: `https://github.com/zksync-sdk/sdk-docs/issues/new?&template=feedback.yml&page=https://sdk.zksync.io${page?.value?._path}&title=[Feedback]%3A%20`,
+      target: '_blank',
+    },
+    {
+      icon: 'i-heroicons-chat-bubble-bottom-center-text',
+      label: 'FAQ',
+      to: `/faq`,
+    },
+    {
+      icon: 'i-heroicons-book-open',
+      label: 'Glossary',
+      to: `/glossary`,
+    },
+    {
+      icon: 'i-heroicons-shield-check',
+      label: 'Troubleshooting',
+      to: `/troubleshooting`,
+    },
+    ...(toc?.bottom?.links || []),
+  ].filter(Boolean)
+);
+</script>
+
+<template>
+  <UPage v-if="page">
+    <UPageHeader
+      :title="page.title"
+      :description="page.description"
+      :links="page.links"
+      :headline="headline"
+    />
+
+    <UPageBody prose>
+      <ContentRenderer
+        v-if="page.body"
+        :value="page"
+      />
+      <hr v-if="surround?.length" />
+
+      <UContentSurround :surround="surround" />
+    </UPageBody>
+
+    <template
+      v-if="page.toc !== false"
+      #right
+    >
+      <UContentToc
+        :title="toc?.title"
+        :links="page.body?.toc?.links"
+      >
+        <template
+          v-if="toc?.bottom"
+          #bottom
+        >
+          <div
+            class="hidden space-y-6 lg:block"
+            :class="{ '!mt-6': page.body?.toc?.links?.length }"
+          >
+            <UDivider
+              v-if="page.body?.toc?.links?.length"
+              type="dashed"
+            />
+
+            <UPageLinks
+              :title="toc.bottom.title"
+              :links="links"
+            />
+          </div>
+        </template>
+      </UContentToc>
+    </template>
+  </UPage>
+</template>
